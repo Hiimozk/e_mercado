ce49eeea076f265d67a01b086da9a8a6cb1be59a
Oscar Nilson <ozkarsad@gmail.com>
2022-08-12 16:39:19 -0300

    loguear con google test2

--- js/login.js
+++ js/login.js
@@ -18,6 +18,7 @@ function onSignIn(googleUser) {
     // The ID token you need to pass to your backend:
     var id_token = googleUser.getAuthResponse().id_token;
     console.log("ID Token: " + id_token);
+    window.location.href = "index2.html";
 }
 
 
